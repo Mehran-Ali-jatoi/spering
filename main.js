325472176195405182e0f8b418f3ee526fdcf709
Mehran-Ali-jatoi <mehran.jato.i258@gmail.com>
2024-11-13 23:52:21 +0500

    A first Project

---
+++ main.js
@@ -0,0 +1,6 @@
+let ul = document.getElementById("ul")
+let bar = document.getElementById("bar")
+bar.addEventListener("click",()=>{
+    ul.classList.toggle("ul")
+    ul.classList.toggle("ani")
+})
